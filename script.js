14bbb8830ab3b3053cac047b9dfd2953c541b7e0
Azazel <trueman898@gmail.com>
2023-06-15 13:33:42 +0300

    code refactoring

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-// rating container
-let ratingContainer = document.querySelector('.rating-window');
-let submitBtn = document.querySelector('.button');
-let ratingScaleNumbers = document.querySelectorAll('.label')
-let ratingScaleNumbersArray = [...ratingScaleNumbers];
+/* ================ Variables ================ */
+const submitBtn = document.querySelector('.button');
+const ratingScaleNumbers = document.querySelectorAll('.label')
+const ratingScaleNumbersArray = [...ratingScaleNumbers];
+const ratingResult = document.querySelector('.selected-rating');
 
-// thank you container
-let thankYouContainer = document.querySelector('.thank-you-window');
-let ratingResult = document.querySelector('.selected-rating');
+/* ================ Functions ================ */
+const showError = (message) => {
+	const error = document.querySelector('.error');
+	error.innerHTML = message;
+}; 
 
-// gets a result rating number 
+const showSuccess = () => {
+	const error = document.querySelector('.error');
+	error.innerHTML = '';
+}
+
+const hideRatingWindow = () => {
+	const ratingContainer = document.querySelector('.rating-window');
+	const thankYouContainer = document.querySelector('.thank-you-window');
+
+	ratingContainer.style.display = 'none';
+	thankYouContainer.style.display = 'flex';
+};
+
+/* ================ Events ================ */
 ratingScaleNumbersArray.forEach((item) => {
-	item.addEventListener('click', function () {
+	item.addEventListener('click', () => {
 		ratingResult.innerHTML = item.innerHTML;
+		showSuccess();
 	});
 });
 
-// hide the rating window
-submitBtn.addEventListener('click', function () {
-	ratingContainer.style.display = 'none';
-	thankYouContainer.style.display = 'flex';
+submitBtn.addEventListener('click', (e) => {
+	e.preventDefault();
+
+	if (ratingResult.innerHTML > 0) {
+		hideRatingWindow();
+	}
+	else {
+		showError('Please, choose an option');
+	}
 });
